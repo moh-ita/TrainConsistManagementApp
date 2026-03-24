42030ca7f947ff13f8532c5e02440ccb99dd68b6
moh-ita <moh-ita@users.noreply.github.com>
2026-03-24 09:33:15 +0530

    UC1: Initialize train consist and display summary

---
+++ UC1InitializeTrain.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UC1InitializeTrain {
+    public static void main(String[] args) {
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("UC1: Initialize Train and Display Consist Summary");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train consist initialized.");
+        System.out.println("Initial bogie count: " + trainConsist.size());
+    }
+}
